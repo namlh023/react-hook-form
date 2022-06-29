8d927683b77e028b3b3cde0756917ea0011c6741
namlh <hoainamle023@gmail.com>
2022-06-29 07:31:52 +0700

    feat: complete ui

--- src/form/index.js
+++ src/form/index.js
@@ -1,9 +1,28 @@
+import React, { useState } from "react";
 import "./form.css";
+import EyeIcon from "../assets/eye.svg";
+import EyeCloseIcon from "../assets/eye-close.svg";
 
 export default function Form() {
+  const [passwordIcon, setPasswordIcon] = useState(EyeIcon);
+  const [passwordType, setPasswordType] = useState("password");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChangePasswordType = (e) => {
+    if (passwordIcon === EyeIcon) {
+      setPasswordIcon(EyeCloseIcon);
+      setPasswordType("text");
+    } else {
+      setPasswordIcon(EyeIcon);
+      setPasswordType("password");
+    }
+  };
+
   return (
     <form className="form">
-      <div className="d-flex gap-8">
+      <div className="d-flex as-start gap-8">
         <input type="checkbox" id="checkbox" />
         <label for="checkbox">Show email and password</label>
       </div>
@@ -20,8 +39,20 @@ export default function Form() {
       <div className="d-flex gap-8 mr-top-16 w-100">
         <input className="w-100" type="email" placeholder="Email" />
       </div>
-      <div className="d-flex gap-8 mr-top-16 w-100">
-        <input className="w-100" type="password" placeholder="Password" />
+      <div className="d-flex gap-8 mr-top-16 w-100 password">
+        <input
+          className="w-100 password__input"
+          type={passwordType}
+          placeholder="Password"
+        />
+        <img
+          type="password"
+          src={passwordIcon}
+          alt=""
+          width="15px"
+          height="12px"
+          onClick={handleChangePasswordType}
+        />
       </div>
       <div className="d-flex gap-8 mr-top-16 w-100">
         <input className="w-100" type="number" placeholder="Minimum age" />
@@ -29,7 +60,7 @@ export default function Form() {
       <div className="d-flex gap- 8 mr-top-16 w-100">
         <input className="w-100" type="number" placeholder="Maximum age" />
       </div>
-      <button className="mr-top-16" type="submit">
+      <button className="mr-top-16" type="submit" onClick={handleSubmit}>
         Submit
       </button>
     </form>
